46d99fa882108fb0f9ca00b39f55b199cb0ece19
tamijuni33@gmail.com <tamijuni33@gmail.com>
2026-01-14 19:34:47 +0200

    Functii hash criptografice

---
+++ Functii hash criptografice/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(98, 65);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Selecteaza Fisier";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(46, 120);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResults.Size = new System.Drawing.Size(188, 74);
+            this.txtResults.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ Functii hash criptografice/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtResults.Clear();
+                    txtResults.AppendText("Se calculează hash-urile." + Environment.NewLine);
+
+                    try
+                    {
+                        string path = ofd.FileName;
+                        string results = await Task.Run(() => GetHashes(path));
+                        txtResults.Text = results;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Eroare: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private string GetHashes(string filePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Rezultate pentru: " + Path.GetFileName(filePath));
+            sb.AppendLine("--------------------------------------------------");
+            sb.AppendLine("MD5:        " + ComputeHash(MD5.Create(), filePath));
+            sb.AppendLine("SHA1:       " + ComputeHash(SHA1.Create(), filePath));
+            sb.AppendLine("SHA256:     " + ComputeHash(SHA256.Create(), filePath));
+            sb.AppendLine("SHA384:     " + ComputeHash(SHA384.Create(), filePath));
+            sb.AppendLine("SHA512:     " + ComputeHash(SHA512.Create(), filePath));
+
+            try
+            {
+                sb.AppendLine("RIPEMD160:  " + ComputeHash(RIPEMD160.Create(), filePath));
+            }
+            catch
+            {
+                sb.AppendLine("RIPEMD160:  Nu este suportat pe acest sistem.");
+            }
+
+            sb.AppendLine("SHA3:       Indisponibil în .NET Framework (necesita .NET 8 sau BouncyCastle)");
+
+            return sb.ToString();
+        }
+
+        private string ComputeHash(HashAlgorithm algorithm, string path)
+        {
+            try
+            {
+                using (algorithm)
+                using (var stream = File.OpenRead(path))
+                {
+                    byte[] hashBytes = algorithm.ComputeHash(stream);
+                    return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Eroare: " + ex.Message;
+            }
+        }
+    }
+}
